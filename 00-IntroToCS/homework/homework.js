d475a3694f2193a2e9c98abd90f50cbd78c91474
Luciano Carducci <carduccilucianof@gmail.com>
2022-04-25 15:39:51 -0300

    Homewrok 00-IntroToCS finished

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,32 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let numalreves = num.split("").reverse().join("");
+  let suma = 0;
+  for (let i = 0; i < numalreves.length; i++) {
+    suma = suma + numalreves[i] * 2 ** i;
+  }
+  return suma;
+
   // algo
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+ let res = new Array(); 
+  while (num != 0){
+    let resto = num % 2
+    num = Math.floor(num/2)
+    res.push(resto)
+  }
+return res.reverse().join("")
 }
+
+// No se pueden usar: 
+// parseInt
+// toString
+
+
 
 module.exports = {
   BinarioADecimal,
