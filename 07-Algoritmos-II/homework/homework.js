1685ab22d5c70bbf0f83cbc1be14f6bfdbb58c2a
Luciano Carducci <carduccilucianof@gmail.com>
2022-05-04 11:32:49 -0300

    Homework 07-Algoritmos-II finished

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
+  var izquierda = [];
+  var derecha = [];
+  var pivote = array[0];
+
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < pivote) {
+      izquierda.push(array[i]);
+    } else {
+      derecha.push(array[i]);
+    }
+  }
+  return quickSort(izquierda).concat(pivote, quickSort(derecha));
 }
 
 function mergeSort(array) {
@@ -14,7 +29,27 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
 
+  var medio = Math.floor(array.length / 2);
+  var izquierda = array.slice(0, medio);
+  var derecha = array.slice(medio);
+
+  derecha = mergeSort(derecha);
+  izquierda = mergeSort(izquierda);
+
+  var arreglo = [];
+
+  while (izquierda.length && derecha.length) {
+    if (izquierda[0] < derecha[0]) {
+      arreglo.push(izquierda.shift());
+    } else {
+      arreglo.push(derecha.shift());
+    }
+  }
+  return arreglo.concat(izquierda, derecha);
 }
 
 // No modificar nada debajo de esta línea
